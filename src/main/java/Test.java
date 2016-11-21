fa12b9e4c310263ff91f9dbc1be479e2d1d477c0
长宏 <3200601@qq.com>
2016-11-21 20:41:00 +0800

    基本完成

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,28 +4,37 @@ import map.test.TestObject;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by 长宏 on 2016/11/21 0021.
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        System.out.println("ABCDEa123abc".hashCode());  // 165374702
+        System.out.println("ABCDFB123abc".hashCode());// 165374702
         for (int i = 0; i < 10; i++) {
             testmymap();
         }
         for (int i = 0; i < 10; i++) {
-            testMapDB();
+//            testMapDB();
         }
 
     }
 
     private static void testmymap() {
         long s = System.currentTimeMillis();
-        TestObject object = new TestObject("金香");
+        TestObject object = new TestObject("金香1");
         MMap mMap = new MyMapImpl("changhong.txt");
-//        mMap.put("jinxiang", object);
-        System.out.println("values is:" + mMap.get("jinxiang") + "    我的 time is:" + (System.currentTimeMillis() - s));
+//        mMap.put("hello1", object);
+        mMap.put("ABCDEa123abc", "a123");
+        mMap.put("ABCDFB123abc", "B123");
+
+        System.out.println("values is:" + mMap.get("ABCDEa123abc") + "    我的 time is:" + (System.currentTimeMillis() - s));
+        System.out.println("values is:" + mMap.get("ABCDFB123abc") + "    我的 time is:" + (System.currentTimeMillis() - s));
     }
 
     private static void testMapDB() {
--- src/main/java/map/test/TestObject.java
+++ src/main/java/map/test/TestObject.java
@@ -1,13 +1,16 @@
 package map.test;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * Created by 长宏 on 2016/11/21 0021.
  */
-public class TestObject implements Serializable{
+public class TestObject implements Externalizable{
     int id = 1;
     String name = "changhong";
+
+    public TestObject() {
+    }
 
     public TestObject(String name) {
         this.name = name;
@@ -15,5 +18,17 @@ public class TestObject implements Serializable{
     @Override
     public String toString() {
         return String.format("id is %s ,name is %s", id, name);
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeInt(id);
+        out.writeUTF(name);
+
+    }
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+        id = in.readInt();
+        name = in.readUTF();
     }
 }
