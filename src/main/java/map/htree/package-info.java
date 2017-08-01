69dc602a6eaabfc04179684f00476b228aa13280
jchanghong <3200601@qq.com>
2017-08-01 13:07:31 +0800

    日志

---
+++ src/main/java/map/htree/package-info.java
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ *    Created on  17-8-1 下午1:06 by jiang
+ *    very fast key value store 简单，快速的键值储存。
+ *    特别为小文件储存设计，比如图片文件。
+ *    把小文件存数据库中不是理想的选择。存在文件系统中又有太多小文件难管理
+ *
+ */
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: jiang
+ * \* Date: 2017/8/1 0001
+ * \* Time: 13:06
+ * \
+ *
+ */
+package map.htree;
+//内存hashtree实现
